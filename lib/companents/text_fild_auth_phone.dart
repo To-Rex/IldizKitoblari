767dc1d505548aa797369e69b673054deb350b8a
to-rex <torex.amaki@gmail.com>
2024-02-20 00:19:09 +0500

    fiest

--- lib/companents/text_fild_auth_phone.dart
+++ lib/companents/text_fild_auth_phone.dart
@@ -22,55 +22,6 @@ class TextFieldPhoneAuth extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*return Container(
-      width: _getController.width.value * 0.92,
-      height: _getController.height.value * 0.056,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(
-          color: Theme.of(context).colorScheme.onSurface.withOpacity(0.3),
-        ),
-      ),
-      child: Obx(() => TextField(
-        controller: nameController,
-        keyboardType: inputType,
-        obscureText: inputType == TextInputType.visiblePassword ? _getController.obscureText.value : false,
-        focusNode: FocusNode(),
-        style: TextStyle(
-          fontSize: _getController.width.value * 0.04,
-          color: Theme.of(context).colorScheme.onSurface,
-        ),
-        decoration: InputDecoration(
-          floatingLabelBehavior: FloatingLabelBehavior.never,
-          hintText: 'Kiriting'.tr,
-          filled: true,
-          isDense: true,
-          fillColor: Theme.of(context).colorScheme.surface,
-          hintStyle: TextStyle(
-            fontSize: _getController.width.value * 0.04,
-            color: Theme.of(context).colorScheme.onSurface.withOpacity(0.4),
-          ),
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,              // No border
-            borderRadius: BorderRadius.circular(12),
-          ),
-          suffixIcon: inputType == TextInputType.visiblePassword ? Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 4, 0),
-            child: GestureDetector(
-              onTap: () {
-                _getController.obscureText.value = !_getController.obscureText.value;
-              },
-              child: Icon(
-                _getController.obscureText.value
-                    ? Icons.visibility_off
-                    : Icons.visibility,
-                size: 24,
-              ),
-            ),
-          ) : null,
-        ),
-      )),
-    );*/
     return Container(
       width: _getController.width.value * 0.92,
       height: _getController.height.value * 0.056,
