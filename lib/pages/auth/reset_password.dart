0f445502cd0e118124c8211fc3349c1cc77dfe24
to-rex <torex.amaki@gmail.com>
2024-02-21 21:48:41 +0500

    fiest

--- lib/pages/auth/reset_password.dart
+++ lib/pages/auth/reset_password.dart
@@ -169,7 +169,7 @@ class ResetPasswordPage extends StatelessWidget {
                                   inputType: TextInputType.visiblePassword,
                                 ),
                                 SizedBox(height: _getController.height.value * 0.02),
-                                if (_getController.countdownDuration.value.inSeconds == 0)
+                                if (_getController.countdownDuration.value.inSeconds == 0 && _getController.fullCheck.value == true && _getController.passwordCheck.value == false)
                                   Row(
                                     children: [
                                       SizedBox(
