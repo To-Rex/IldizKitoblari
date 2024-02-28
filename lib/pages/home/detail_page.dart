7dd253758d13f62a296e438aa87707a960cde38e
to-rex <torex.amaki@gmail.com>
2024-02-29 01:07:19 +0500

    fiest

--- lib/pages/home/detail_page.dart
+++ lib/pages/home/detail_page.dart
@@ -11,9 +11,9 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail'),
+        title: const Text('Detail'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Detail Page'),
       ),
     );
