16bd86166bc5234f9b62314e02bbd96393739575
to-rex <torex.amaki@gmail.com>
2024-03-01 03:12:25 +0500

    fiest

--- lib/pages/home/category_page.dart
+++ lib/pages/home/category_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ildiz/pages/home/detail_page.dart';
@@ -15,26 +16,39 @@ class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Kategoriya'.tr),
+        centerTitle: false,
+        toolbarTextStyle: TextStyle(fontSize: _getController.width.value * 0.048, fontWeight: FontWeight.w600),
+        surfaceTintColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,
+            color: Theme.of(context).colorScheme.onBackground,
+            size: _getController.width.value * 0.06,
+          ),
+          onPressed: () {
+            Get.back();
+          },
+        )
+      ),
       body: Obx(() => _getController.menuModel.value.data!.result![menuIndex].children == null
-                ? Center(child: Text('Ma\'lumotlar yo\'q!'.tr, style: TextStyle(fontSize: _getController.width.value * 0.04, fontWeight: FontWeight.w600)))
-                : Column(
+          ? Center(child: Text('Ma\'lumotlar yo\'q!'.tr, style: TextStyle(fontSize: _getController.width.value * 0.04, fontWeight: FontWeight.w600)))
+          : Column(
         children: [
-          MyAppBar(title: 'Kategoriya'),
+          //MyAppBar(title: 'Kategoriya'),
           Expanded(
             child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
               itemCount: _getController.menuModel.value.data!.result?[menuIndex].children!.length,
-              itemBuilder: (context, index) {
-                return CategoryItem(
-                  _getController.menuModel.value.data!.result![menuIndex].children![index].sId!,
-                  'uz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.uz! : 'oz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.oz! : _getController.menuModel.value.data!.result![menuIndex].children![index].title!.ru!, () {Get.to(() => DetailPage(title: 'uz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.uz! : 'oz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.oz! : _getController.menuModel.value.data!.result![menuIndex].children![index].title!.ru!, menuSlug: _getController.menuModel.value.data!.result![menuIndex].children![index].slug!));},
-                );
-              },
-            ),
-          ),
-        ],
+                itemBuilder: (context, index) {
+                  return CategoryItem(
+                      _getController.menuModel.value.data!.result![menuIndex].children![index].sId!,
+                      'uz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.uz! : 'oz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.oz! : _getController.menuModel.value.data!.result![menuIndex].children![index].title!.ru!, () {Get.to(() => DetailPage(title: 'uz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.uz! : 'oz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].title!.oz! : _getController.menuModel.value.data!.result![menuIndex].children![index].title!.ru!, menuSlug: _getController.menuModel.value.data!.result![menuIndex].children![index].slug!));}
+                  );
+                }
+            )
+          )
+        ])
       )
-      ),
     );
   }
 }
