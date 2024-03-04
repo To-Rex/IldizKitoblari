3fbeaf2a883abd092fd7ac28398b86b07b837388
to-rex <torex.amaki@gmail.com>
2024-03-04 22:05:26 +0500

    fiest

--- lib/pages/home/sub_category_page.dart
+++ lib/pages/home/sub_category_page.dart
@@ -1467,26 +1467,24 @@ class SubCategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('shuuuuu ${_getController.menuModel.value.data!.result![menuIndex].children![index]!.children![index]!.title!.uz}');
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: Obx(() => _getController.menuModel.value.data!.result![menuIndex].children![menuIndex]! == null
+        body: Obx(() => _getController.menuModel.value.data!.result![menuIndex].children![menuIndex].children != null
             ? Center(child: Text('Ma\'lumotlar yo\'q!'.tr, style: TextStyle(fontSize: _getController.width.value * 0.04, fontWeight: FontWeight.w600)))
             : Column(
             children: [
               Expanded(
                 child: ListView.builder(
-                    itemCount: _getController.menuModel.value.data!.result![menuIndex].children![index]!.children!.length,
+                    itemCount: _getController.menuModel.value.data!.result![menuIndex].children![index].children!.length,
                     itemBuilder: (context, indexs) {
                       return CategoryItem(
                           _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].sId!,
                           'uz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].title!.uz! : 'oz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].title!.oz! : _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].title!.ru!, () {
-                        _getController.page.value = 1;
-                        _getController.productModelLength.value = 0;
-                        _getController.clearProductModel();
-                        print('menuIndex: $menuIndex');
+                            _getController.page.value = 1;
+                            _getController.productModelLength.value = 0;
+                            _getController.clearProductModel();
                         Get.to(() => CatDetailPage(title: 'uz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].title!.uz! : 'oz_UZ' == Get.locale.toString() ? _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].title!.oz! : _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].title!.ru!, menuSlug: _getController.menuModel.value.data!.result![menuIndex].children![index].children![indexs].slug!));
                       });
                     }
