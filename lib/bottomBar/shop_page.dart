963611aa056815591aacec6316d65747f34e572b
to-rex <torex.amaki@gmail.com>
2024-03-11 18:07:04 +0500

    fiest

--- lib/bottomBar/shop_page.dart
+++ lib/bottomBar/shop_page.dart
@@ -19,68 +19,7 @@ class ShopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*return Scaffold(
-        body: SingleChildScrollView(
-          child: SizedBox(
-              height: _getController.height.value,
-              width: _getController.width.value,
-              child: Obx(() => Stack(
-                  fit: StackFit.loose,
-                  children: [
-                    Positioned(
-                        height: _getController.height.value * 0.24,
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        child: SizedBox(
-                          child: SvgPicture.asset('assets/svgImages/shap.svg',
-                              fit: BoxFit.fitWidth,
-                              width: _getController.width.value,
-                              height: _getController.height.value * 0.3),
-                        )),
-                    Positioned(
-                        height: _getController.height.value * 0.2,
-                        top: _getController.height.value * 0.062,
-                        left: _getController.width.value * 0.03,
-                        child: Text('Do\'kon'.tr,
-                            style: TextStyle(
-                                color: Theme.of(context).colorScheme.surface,
-                                fontSize: _getController.width.value * 0.061,
-                                fontWeight: FontWeight.bold
-                            )
-                        )
-                    ),
-                    Positioned(
-                        height: _getController.height.value * 0.2,
-                        top: _getController.height.value * 0.02,
-                        left: 0,
-                        right: 0,
-                        child: SearchFields()
-                    ),
-                    Positioned(
-                        width: _getController.width.value,
-                        top: _getController.height.value * 0.19,
-                        bottom: 0,
-                        child: Container(
-                            width: _getController.width.value,
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.background,
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(16),
-                                topRight: Radius.circular(16),
-                              ),
-                            ),
-                            child: Column(
-                              children: [
-                              ],
-                            )
-                        )
-                    )
-                  ]
-              ))
-          ),
-        )
-    );*/
+    _getController.changeItemPage(2);
     return Scaffold(
         body: SmartRefresher(
             enablePullDown: true,
@@ -107,6 +46,8 @@ class ShopPage extends StatelessWidget {
               },
             ),
             onLoading: () async {
+              print('onLoading');
+              _getController.addItemPage();
               _refreshController.loadComplete();
             },
             onRefresh: () async {
@@ -142,7 +83,7 @@ class ShopPage extends StatelessWidget {
                               child:Column(
                                   children: [
                                     if (_getController.menuModel.value.data != null)
-                                      for (var i in _getController.menuModel.value.data!.result!.length > 2 ? _getController.menuModel.value.data!.result!.sublist(0, 2) : _getController.menuModel.value.data!.result!)
+                                      for (var i in _getController.menuModel.value.data!.result!.length > _getController.itemPage.value ? _getController.menuModel.value.data!.result!.sublist(0, _getController.itemPage.value) : _getController.menuModel.value.data!.result!)
                                         Column(
                                             children: [
                                               ChildItem(title: 'uz_UZ' == Get.locale.toString() ? i.title!.uz! : 'oz_OZ' == Get.locale.toString() ? i.title!.oz! : i.title!.ru!, function: (){}),
