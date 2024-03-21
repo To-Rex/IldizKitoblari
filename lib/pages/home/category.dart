e8932ec22bdfdef3f75b1c46ab33f21e7f9a1b44
to-rex <torex.amaki@gmail.com>
2024-03-21 17:47:59 +0500

    fiest

--- lib/pages/home/category.dart
+++ lib/pages/home/category.dart
@@ -13,38 +13,35 @@ class Category extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Kategoriyalar',
-            style: TextStyle(fontSize: _getController.width.value * 0.05, fontWeight: FontWeight.w500, color: Theme.of(context).colorScheme.onBackground)),
-        centerTitle: false,
-        elevation: 0,
-        surfaceTintColor: Colors.transparent,
-        backgroundColor: Theme.of(context).colorScheme.background,
-        leading: IconButton(icon: Icon(Icons.arrow_back, color: Theme.of(context).colorScheme.onBackground, size: _getController.width.value * 0.06), onPressed: () {Get.back();})
-      ),
-      body: Obx(() => _getController.menuModel.value.data == null
-          ? Center(child: Text('Ma\'lumotlar yo\'q!'.tr, style: TextStyle(fontSize: _getController.width.value * 0.04, fontWeight: FontWeight.w600)))
-          : SizedBox(
-        width: _getController.width.value,
-        height: _getController.height.value,
-        child: ListView.builder(
-          itemCount: _getController.menuModel.value.data == null ? 0 : _getController.menuModel.value.data!.result!.length,
-          itemBuilder: (context, index) {
-            return CategoryItem(
-              _getController.menuModel.value.data!.result![index].sId!,
-              _getController.menuModel.value.data!.result![index].slug!,
-              () {
-                if (_getController.menuModel.value.data!.result![index].children == null) {
-                  Get.to(() => CatDetailPage(title: _getController.menuModel.value.data!.result![index].title!.uz!, menuSlug: _getController.menuModel.value.data!.result![index].slug!));
-                }else {
-                  Get.to(() => CategoryPage(menuIndex: _getController.menuModel.value.data!.result!.indexOf(_getController.menuModel.value.data!.result![index])));
+        appBar: AppBar(
+            title: Text('Kategoriya'.tr),
+            centerTitle: false,
+            toolbarTextStyle: TextStyle(fontSize: _getController.width.value * 0.048, fontWeight: FontWeight.w600),
+            surfaceTintColor: Colors.transparent,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Theme.of(context).colorScheme.onBackground, size: _getController.width.value * 0.06),
+              onPressed: () {Get.back();}
+            )
+        ),
+        body: Obx(() => _getController.menuModel.value.data == null
+            ? Center(child: Text('Ma\'lumotlar yo\'q!'.tr, style: TextStyle(fontSize: _getController.width.value * 0.04, fontWeight: FontWeight.w600)))
+            : SizedBox(
+            width: _getController.width.value,
+            height: _getController.height.value,
+            child: ListView.builder(
+                itemCount: _getController.menuModel.value.data == null ? 0 : _getController.menuModel.value.data!.result!.length,
+                itemBuilder: (context, index) {
+                  return CategoryItem(
+                      _getController.menuModel.value.data!.result![index].sId!,
+                      _getController.menuModel.value.data!.result![index].slug!,
+                          () {
+                        if (_getController.menuModel.value.data!.result![index].children == null) {
+                          Get.to(() => CatDetailPage(title: _getController.menuModel.value.data!.result![index].title!.uz!, menuSlug: _getController.menuModel.value.data!.result![index].slug!));
+                        }else {
+                          Get.to(() => CategoryPage(menuIndex: _getController.menuModel.value.data!.result!.indexOf(_getController.menuModel.value.data!.result![index])));
+                        }
+                      });
                 }
-              }
-            );
-          }
-        )
-      )
-      )
-    );
+                ))));
   }
 }
