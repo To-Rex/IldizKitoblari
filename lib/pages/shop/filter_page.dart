69ee81e4af5f1c053d027e910fac762b96939e8b
to-rex <torex.amaki@gmail.com>
2024-05-17 23:44:23 +0500

    fiest

--- lib/pages/shop/filter_page.dart
+++ lib/pages/shop/filter_page.dart
@@ -43,17 +43,11 @@ class FilterPage extends StatelessWidget{
             surfaceTintColor: Colors.transparent,
             leading: IconButton(
                 icon: Icon(Icons.arrow_back, color: Theme.of(context).colorScheme.onBackground, size: _getController.width.value * 0.06),
-                onPressed: () {
-                  Get.back();
-                }
+                onPressed: () {Get.back();}
             ),
             actions: [
               TextButton(
-                  onPressed: () {
-                    _getController.clearFilters();
-                    debugPrint(_getController.textControllers.length.toString());
-                    debugPrint(menuSlug.toString());
-                  },
+                  onPressed: () {_getController.clearFilters();},
                   child: Text('Filterlarni o\'chirish'.tr, style: TextStyle(color: AppColors.red, fontSize: 18.sp, fontWeight: FontWeight.w500))),
               SizedBox(width: 10.sp)
             ]
@@ -67,24 +61,16 @@ class FilterPage extends StatelessWidget{
                 if (mode == RefreshStatus.idle) {
                   body = const Text("Ma`lumotlarni yangilash uchun tashlang");
                 } else if (mode == RefreshStatus.refreshing) {
-                  body = const CircularProgressIndicator(
-                    color: Colors.blue,
-                    backgroundColor: Colors.white,
-                    strokeWidth: 2,
-                  );
+                  body = const CircularProgressIndicator(color: Colors.blue, backgroundColor: Colors.white, strokeWidth: 2);
                 } else if (mode == RefreshStatus.failed) {
-                  body = const Text("Ex nimadir xato ketdi",
-                      style: TextStyle(fontSize: 14, color: Colors.red));
+                  body = const Text("Ex nimadir xato ketdi", style: TextStyle(fontSize: 14, color: Colors.red));
                 } else if (mode == RefreshStatus.canRefresh) {
                   body = const Text("Ma`lumotlarni yangilash uchun tashlang");
                 } else {
                   body = const Text("Ma`lumotlar yangilandi");
                 }
-                return SizedBox(
-                  height: _getController.height.value * 0.1,
-                  child: Center(child: body),
-                );
-              },
+                return SizedBox(height: _getController.height.value * 0.1, child: Center(child: body));
+              }
             ),
             footer: CustomFooter(
               builder: (BuildContext context, LoadStatus? mode) {
@@ -100,11 +86,8 @@ class FilterPage extends StatelessWidget{
                 } else {
                   body = const Text("Ma`lumotlar yangilandi", style: TextStyle(fontSize: 14, color: Colors.black));
                 }
-                return SizedBox(
-                  height: _getController.height.value * 0.1,
-                  child: Center(child: body),
-                );
-              },
+                return SizedBox(height: _getController.height.value * 0.1, child: Center(child: body));
+              }
             ),
             onLoading: () async {_refreshController.loadComplete();},
             onRefresh: () async {_refreshController.refreshCompleted();},
@@ -120,13 +103,12 @@ class FilterPage extends StatelessWidget{
                             function: (int value) {
                               if (_getController.genreIndex.value == 1) {
                                 _getController.changeGenreListSelect(value);
-                                print('${_getController.filterGenre}');
                               } else if (_getController.genreIndex.value == 2) {
                                 _getController.changeGenreListSelect(value);
                               }
                             },
                             select: _getController.genreIndex.value == 0 ? null : _getController.filterGenre[menuIndex],
-                            icon: Icons.keyboard_arrow_down_outlined,
+                            icon: Icons.keyboard_arrow_down_outlined
                         ),
                       Container(margin: EdgeInsets.only(top: _getController.width.value * 0.04,left: _getController.width.value * 0.04, right: _getController.width.value * 0.04), child: Divider(height: 1, color: Theme.of(context).colorScheme.onBackground.withOpacity(0.2))),
                       Container(width: double.infinity, margin: EdgeInsets.only(left: _getController.width.value * 0.04, right: _getController.width.value * 0.04, top: _getController.width.value * 0.04,bottom: _getController.width.value * 0.01), child: Text('Narx', style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500))),
@@ -162,7 +144,7 @@ class FilterPage extends StatelessWidget{
                                       focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10.r), borderSide: BorderSide(color: Theme.of(context).colorScheme.background, width: 0)),
                                       contentPadding: EdgeInsets.symmetric(horizontal: _getController.width.value * 0.02, vertical: _getController.height.value * 0.008),
                                       hintText: '0 gacha'.tr,
-                                      hintStyle: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 17.sp, fontWeight: FontWeight.w400),
+                                      hintStyle: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 17.sp, fontWeight: FontWeight.w400)
                                     )
                                 )
                             )
@@ -204,7 +186,7 @@ class FilterPage extends StatelessWidget{
                                             focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10.r), borderSide: BorderSide(color: Theme.of(context).colorScheme.background, width: 0)),
                                             contentPadding: EdgeInsets.symmetric(horizontal: _getController.width.value * 0.02, vertical: _getController.height.value * 0.008),
                                             hintText: 'Kiriting'.tr,
-                                            hintStyle: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 17.sp, fontWeight: FontWeight.w400),
+                                            hintStyle: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 17.sp, fontWeight: FontWeight.w400)
                                           )
                                       )
                                   )
