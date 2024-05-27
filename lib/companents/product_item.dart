122441ea0006ed6d9281c0e330fd25cb304a0702
to-rex <torex.amaki@gmail.com>
2024-05-27 23:38:51 +0500

    fiest

--- lib/companents/product_item.dart
+++ lib/companents/product_item.dart
@@ -42,10 +42,7 @@ class _ProductItemState extends State<ProductItem> {
         child: Obx(() => Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                  width: 185.sp, height: 190.sp,
-                  child: ClipRRect(borderRadius: BorderRadius.circular(4), child: Image.network(widget.imageUrl == null || widget.imageUrl == '' || widget.imageUrl == 'null' || widget.imageUrl == ' ' ? 'https://ildizkitoblari.uz/_ipx/w_300,f_webp/default.png' : widget.imageUrl!, fit: BoxFit.cover, loadingBuilder: (context, child, loadingProgress) {if (loadingProgress == null) return child;return SizedBox(width: _getController.width.value * 0.44, height: _getController.height.value * 0.205, child: Skeletonizer(child: Container(width: _getController.width.value * 0.44, height: _getController.height.value * 0.205, color: AppColors.grey)));}, errorBuilder: (context, error, stackTrace) {return Text(error.toString());}))
-              ),
+              SizedBox(width: 185.sp, height: 190.sp, child: ClipRRect(borderRadius: BorderRadius.circular(4), child: Image.network(widget.imageUrl == null || widget.imageUrl == '' || widget.imageUrl == 'null' || widget.imageUrl == ' ' ? 'https://ildizkitoblari.uz/_ipx/w_300,f_webp/default.png' : widget.imageUrl!, fit: BoxFit.cover, loadingBuilder: (context, child, loadingProgress) {if (loadingProgress == null) return child;return SizedBox(width: _getController.width.value * 0.44, height: _getController.height.value * 0.205, child: Skeletonizer(child: Container(width: _getController.width.value * 0.44, height: _getController.height.value * 0.205, color: AppColors.grey)));}, errorBuilder: (context, error, stackTrace) {return Text(error.toString());}))),
               SizedBox(height: 10.sp),
               Container(
                   margin: EdgeInsets.only(right: 5.sp),
