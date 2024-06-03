0fd822b73452c67f75f34e69db218c253776a168
to-rex <torex.amaki@gmail.com>
2024-06-04 03:43:51 +0500

    fiest

--- lib/pages/order/order_confirmation_page.dart
+++ lib/pages/order/order_confirmation_page.dart
@@ -151,9 +151,7 @@ class OrderConfirmationPage extends StatelessWidget {
                         style: ButtonStyle(backgroundColor: MaterialStateProperty.all(AppColors.primaryColor2), shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.r)))),
                         onPressed: (){
                          // m=664c890b70ee2ef365a80e85;ac.order_id=TESTORDER;ac.full_name=Aziz Kuchkarov;ac.phone=+998 (93) 556-57-41;a=5000000
-                          //base64 encode
-                          //var data = base64.encode(utf8.encode('m=664c890b70ee2ef365a80e85;ac.order_id=TESTORDER;ac.full_name=${_getController.meModel.value.data!.result?.fullName};ac.phone=+998 (93) 556-57-41;a=5000000'));
-                          var data = base64.encode(utf8.encode('m=664c890b70ee2ef365a80e85;ac.order_id=TESTORDER;ac.full_name=${_getController.meModel.value.data!.result?.fullName};ac.phone=${_getController.meModel.value.data!.result?.phone};a=${int.parse(_getController.orderDetailModel.value.data!.price.toString()) + int.parse(_getController.orderDetailModel.value.data!.deliveryPrice.toString())}'));
+                          var data = base64.encode(utf8.encode('m=664c890b70ee2ef365a80e85;ac.order_id=TESTORDER;ac.full_name=${_getController.meModel.value.data!.result?.fullName};ac.phone=${_getController.meModel.value.data!.result?.phone};a=${int.parse(_getController.orderDetailModel.value.data!.price.toString()) + int.parse(_getController.orderDetailModel.value.data!.deliveryPrice.toString())}00'));
                           print('https://checkout.paycom.uz/$data');
                           print(_getController.meModel.value.data!.result?.phone);
                           launchUrl(Uri.parse('https://checkout.paycom.uz/$data'));
