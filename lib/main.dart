f54ceae0154ffdcdd27b42abb64a3569997b0e30
to-rex <torex.amaki@gmail.com>
2024-06-05 02:11:14 +0500

    fiest

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:ildiz/pages/splash_screen.dart';
 import 'package:ildiz/resource/srting.dart';
-
 import 'controllers/get_controller.dart';
 
 
