54cbe3e9262a75526e675954976e4aefce934b10
to-rex <torex.amaki@gmail.com>
2024-06-24 01:59:15 +0500

    fiest

--- lib/pages/profile/about_program.dart
+++ lib/pages/profile/about_program.dart
@@ -16,36 +16,39 @@ class AboutProgram extends StatelessWidget{
     ApiController().getAbout();
     return Scaffold(
         appBar: AppBar(
-            title: Text('About the program', style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500)),
+            title: Text('Dastur haqida'.tr, style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500)),
             centerTitle: false,
             surfaceTintColor: Colors.transparent,
             leading: IconButton(
               icon: Icon(Icons.arrow_back, color: Theme.of(context).colorScheme.onSurface, size: 20.sp),
               onPressed: () {
                 Get.back();
-              },
-            )),
+              }
+            )
+        ),
         body: Obx(() =>
             SingleChildScrollView(
                 child: Column(
                     children: [
                       if (_getController.aboutModel.value.data?.content?.uz != null)
-                        Html(style: {'p': Style(
-                      fontSize: FontSize(16.sp),
-                      fontWeight: FontWeight.w400, color: Theme.of(context).colorScheme.onSurface)},
-                            data: _getController.aboutModel.value.data?.content?.uz ?? ''
+                        Container(
+                            margin: EdgeInsets.only(top: _getController.width.value * 0.04, left: _getController.width.value * 0.03,right: _getController.width.value * 0.03),
+                            child: Html(
+                                style: {'p': Style(textAlign: TextAlign.justify, fontSize: FontSize(16.sp), fontWeight: FontWeight.w400, color: Theme.of(context).colorScheme.onSurface)},
+                                data: _getController.aboutModel.value.data?.content?.uz ?? ''
+                            )
                         )
                       else
                         if (!_getController.onLoading.value)
                           SizedBox(
                             height: Get.height * 0.8,
                             width: Get.width,
-                            child: const Center(
-                                child: CircularProgressIndicator()
-                            )
+                            child: const Center(child: CircularProgressIndicator())
                           )
                         else
-                          Container(),
+                          Container(
+
+                          )
                     ]
                 )
             )
